0e636e46a2b05a0d821e32bb32e0b7bf048a7a03
Lucca Nunes <luccanunesgit@gmail.com>
2020-07-28 11:42:52 -0300

    live command working

---
+++ src/live.py
@@ -0,0 +1,39 @@
+def get_lives():
+    from selenium import webdriver
+    from time import sleep
+    # TODO add streamer name/link, language, viewers
+
+    URL = "https://www.twitch.tv/directory/game/Tom%20Clancy's%20Rainbow%20Six%3A%20Siege"
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("--start-maximized")
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(
+        executable_path=r'E:\coding\python\chromedriver.exe', chrome_options=options
+    )
+    driver.get(URL)
+    sleep(3)
+
+    # * LIVE STREAMERS - document.getElementsByClassName('tw-c-text-alt-2')[12:16]
+    streamers = driver.find_elements_by_class_name(
+        'tw-c-text-alt-2')[6:]  # ! CHANGE HERE TO GET OTHER STREAMERS
+    lives = list()
+    for streamer in streamers:
+        lives.append(streamer.text)
+
+    return lives
+
+
+def format_lives(lives):
+    import discord
+    msg = discord.Embed(
+        # title='Live Streams',
+        colour=discord.Color.from_rgb(100, 65, 165)
+    )
+    msg.set_thumbnail(url='https://i.imgur.com/wLPYW8A.png')
+    for live in lives:
+        # msg.add_field(name=live, value=f'https://www.twitch.tv/{live}')
+        msg.add_field(name=live, value=f'[watch](https://www.twitch.tv/{live})')
+
+    return msg
+
--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from ops import ops
 from format_op import format_op
 from unidecode import unidecode
 from player_info import get_player_info, format_player_info
+from live import get_lives, format_lives
 
 client = discord.Client()
 
@@ -24,14 +25,17 @@ async def on_message(message):
     if message.content.lower().startswith(f'{prefix}'):
         if message.content.lower().startswith(f'{prefix}help'):
             msg = discord.Embed(title='Commands', description="Here's a list of all commands!", colour=discord.Color.from_rgb(244, 175, 44))
+            msg.set_thumbnail(url='https://i.imgur.com/Vgrtz5u.png')
             msg.add_field(name=f'{prefix}pcstats', value='shows info regarding the status of the PC servers')
             msg.add_field(name=f'{prefix}ps4stats', value='shows info regarding the status of the PS4 servers')
             msg.add_field(name=f'{prefix}xboxstats', value='shows info regarding the status of the Xbox One servers')
             msg.add_field(name=f'{prefix}agent (agent name)', value="shows agent's general info")
             msg.add_field(name=f'{prefix}player (name) (platform)', value="tracks player's info")
+            msg.add_field(name=f'{prefix}live', value=f"shows live streams on twitch ({prefix}lives also works)")
             await message.channel.send(embed=msg)
         elif 'stats' in message.content.lower():
-            await message.channel.send("```Getting info...```")
+            # await message.channel.send("```Getting info...```")
+            await message.channel.send("Working on it...")
             if message.content.lower().startswith(f'{prefix}pcstats'):
                 status = get_server_status()
                 info = format_dict(status["pc"], status['last-update'])
@@ -45,7 +49,8 @@ async def on_message(message):
                 info = format_dict(status["xbox"], status['last-update'])
                 await message.channel.send(embed=info)
         elif message.content.lower().startswith(f'{prefix}news'):
-            await message.channel.send("```Getting info...```")
+            # await message.channel.send("```Getting info...```")
+            await message.channel.send("Working on it...")
             news = get_news()
             string = ''
             for new, date in news.items():
@@ -70,7 +75,8 @@ async def on_message(message):
                 await message.channel.send(embed=msg)
             else:
                 try:
-                    await message.channel.send("```Getting info...```")
+                    # await message.channel.send("```Getting info...```")
+                    await message.channel.send("Working on it...")
                     player_info = format_player_info(get_player_info(platform, player))
                     await message.channel.send(embed=player_info["overview"])
                     await message.channel.send(embed=player_info["ranked"])
@@ -78,5 +84,11 @@ async def on_message(message):
                 except:
                     msg = discord.Embed(title='Error', description=f"Failed to find {player}'s information! Please check player's name and platform and try again", colour=discord.Color.from_rgb(255, 0, 0))
                     await message.channel.send(embed=msg)
+        elif message.content.lower().startswith(f'{prefix}lives') or message.content.lower().startswith(f'{prefix}live'):
+            # await message.channel.send("```Getting info...```")
+            await message.channel.send("Working on it...")
+            lives = get_lives()[:6]
+            msg = format_lives(lives)
+            await message.channel.send(embed=msg)
 
 client.run('NzM2NzY2NDg0MDU5MjU4OTMx.XxzlQg._8lktwlNtqNccRRO8vu62LQwHQI')
