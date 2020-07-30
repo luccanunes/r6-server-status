c62031af3561d27c415088af00b7c13c75892ee0
Lucca Nunes <luccanunesgit@gmail.com>
2020-07-30 17:39:58 -0300

    Update __init__.py

--- src/packages/quote/__init__.py
+++ src/packages/quote/__init__.py
@@ -3,8 +3,7 @@ def get_quote():
     generates a random very wise quote
     '''
     from random import randint
-    # quotes = ['tr7iu5', 'tr7j1o', 'tr7jo9', 'tr7kol', 'tr91t5']
-    quotes = ['tr7iu5', 'tr7j1o', 'tr7jo9', 'tr7kol', 'tr97x9']
+    quotes = ['tr7iu5', 'tr7j1o', 'tr7jo9', 'tr7kol', 'trep2v']
     domain = 'https://prnt.sc/'
     
     return domain + quotes[randint(0, len(quotes)-1)] 
